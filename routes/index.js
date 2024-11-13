b9308c37c805fba82c3ed23aea29123b335ce83f
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-11-13 16:51:22 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
-let movie = {};
 router.get(
 	"/movies",
 	(req, res) => {
 		fetch(
-			`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
+			`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.KEY}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
 				res.json({ movies: data.results });
-			})
-			.then((res) => console.log(res))
-			.catch((err) => console.error(err));
+			});
 	},
 	[]
 );
